12513b95435dc4e16474aca33c11b54e131d8098
Kros Dai <kros@dai.sg>
2023-03-03 22:05:38 +0800

    Update Zsh, Git, and global gitignore for macOS
    - Update Anaconda path to `/opt/homebrew/anaconda3/`
    - Add `/opt/homebrew/anaconda3/bin` to PATH
    - Add multiple files to global gitignore
    - Update alias helper path in gitconfig
    - Enable commit signing in gitconfig
    
    [zsh/zshrc]
    - Change the location of the `envs.zsh` file from `$ZDOTDIR/../.dotlocal/envs.zsh` to `$ZDOTDIR/../../.dotlocal/envs.zsh`
    - Add `$DOTFILES/bin` to `$PATH`
    [zsh/os.Darwin.zsh]
    - Update Anaconda path from `/opt/anaconda/` to `/opt/homebrew/anaconda3/`
    - Update `conda init` hook path from `/opt/anaconda/bin/conda` to `/opt/homebrew/anaconda3/bin/conda`
    - Add `/opt/homebrew/anaconda3/bin` to the PATH environment variable
    [git/gitignore_global]
    - Add `.vscode/*` to global gitignore
    - Add `*.code-workspace` to global gitignore
    - Add common credential files to global gitignore
    - Add private node modules to global gitignore
    - Add private files to global gitignore
    - Add Mac/OSX files to global gitignore
    - Add dump files to global gitignore
    - Add folder config files to global gitignore
    - Add recycle bin files to global gitignore
    - Add
    [git/gitconfig]
    - Remove signing key
    - Remove `gpg "ssh"` section
    - Enable commit signing
    - Update alias helper path
    - Set `useHttpPath` to `true` for Azure credential

--- zsh/os.Darwin.zsh
+++ zsh/os.Darwin.zsh
@@ -46,14 +46,14 @@ export PKG_CONFIG_PATH="$HOMEBREW/opt/ruby/lib/pkgconfig"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/anaconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/opt/anaconda/etc/profile.d/conda.sh" ]; then
-        . "/opt/anaconda/etc/profile.d/conda.sh"
+    if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
     else
-        path=("/opt/anaconda/bin" $path)
+        export PATH="/opt/homebrew/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
