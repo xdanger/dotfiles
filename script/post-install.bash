acfb56064e25634b12f4b3d1e878bfd00244992b
Yunjie Dai <xdanger@gmail.com>
2025-05-26 23:15:49 +0800

    🔧 Configure SSH signing for Git commits and improve dotfiles setup
    Add SSH signing key and configure GPG format to use SSH for commit
    signing. Comment out Zed config symlink and enhance shell command
    configuration with explicit stdout/stderr handling. Refactor
    post-install script to improve readability by reorganizing command
    order, simplifying WSL detection logic, and grouping macOS-specific
    operations under a conditional block.

--- script/post-install.bash
+++ script/post-install.bash
@@ -1,24 +1,19 @@
 # init git submodules
 git submodule update --init --recursive --force
+# A command-line finder
+./fzf/install --bin
+# install diff-so-fancy
+cp ./diff-so-fancy/diff-so-fancy ./bin/diff-so-fancy && cp ./diff-so-fancy/lib/* ./bin/lib/
 
 # link ~/.gitconfig to gitconfig_codespaces in GitHub Codespaces
 [[ $CODESPACES == "true" ]] && ln -sf $PWD/git/gitconfig.codespaces ~/.gitconfig
-
 # link ~/.gitconfig to gitconfig.wsl in Windows Subsystem for Linux
-is_wsl() {
-  [[ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]] && return 0
-  # [[ $(<"/proc/sys/kernel/osrelease") == *[Mm]icrosoft* ]] && return 0
-  [[ -n ${WSL_DISTRO_NAME:-} ]] && return 0
-  return 1
-}
-is_wsl && ln -sf $PWD/git/gitconfig.wsl ~/.gitconfig
+[[ -n ${WSL_DISTRO_NAME:-} ]] && ln -sf $PWD/git/gitconfig.wsl ~/.gitconfig
 
-./fzf/install --bin
-
-cp ./diff-so-fancy/diff-so-fancy ./bin/diff-so-fancy && cp ./diff-so-fancy/lib/* ./bin/lib/
-
-[[ `uname` == "Darwin" ]] && clang -framework Carbon util/reset-input.m -o bin/reset-input
-
-# brew update && brew upgrade
-# brew install nodejs bat prettyping htop diff-so-fancy ncdu tldr ack ag fortune ponysay csvkit noti entr youtube-dl aria2 yt-dlp
-# brew tap homebrew/cask-fonts && brew install -f font-fira-code
+# macOS
+if [[ `uname` == "Darwin" ]]; then
+  clang -framework Carbon util/reset-input.m -o bin/reset-input
+  # brew update && brew upgrade
+  # brew install nodejs bat prettyping htop diff-so-fancy ncdu tldr ack ag fortune ponysay csvkit noti entr youtube-dl aria2 yt-dlp
+  # brew tap homebrew/cask-fonts && brew install -f font-fira-code
+fi
