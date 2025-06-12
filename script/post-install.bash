c66bfd995d296af8ea7e69e2c5b978ed507212d1
Yunjie Dai <xdanger@gmail.com>
2025-06-12 19:14:31 +0800

    🔧 Replace diff-so-fancy with delta for improved git diff display
    Replace diff-so-fancy with delta as the git pager and diff filter to
    provide better syntax highlighting and navigation features. Update
    merge conflict style to zdiff3 for clearer conflict resolution. Enable
    worktree relative paths and configure delta navigation. Update macOS
    installation script to include delta and other modern CLI tools while
    removing outdated packages.

--- script/post-install.bash
+++ script/post-install.bash
@@ -13,7 +13,7 @@ cp ./diff-so-fancy/diff-so-fancy ./bin/diff-so-fancy && cp ./diff-so-fancy/lib/*
 # macOS
 if [[ `uname` == "Darwin" ]]; then
   clang -framework Carbon util/reset-input.m -o bin/reset-input
-  # brew update && brew upgrade
-  # brew install nodejs bat prettyping htop diff-so-fancy ncdu tldr ack ag fortune ponysay csvkit noti entr youtube-dl aria2 yt-dlp
+  brew update && brew upgrade
+  brew install ack ag aria2 bat csvkit diff-so-fancy entr fortune git-delta gitkraken-cli glab htop ncdu noti ripgrep prettyping tldr yt-dlp
   # brew tap homebrew/cask-fonts && brew install -f font-fira-code
 fi
